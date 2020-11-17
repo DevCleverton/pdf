98be79f04bbc90305b2944a22dc5071e7c0c6bd5
Cleverton da Rocha <56972170+DevCleverton@users.noreply.github.com>
2020-11-17 00:23:38 -0300

    Add files via upload

---
+++ pdf/elements/viewer-bookmarks-content/viewer-bookmarks-content.js
@@ -0,0 +1,1 @@
+Polymer({is: 'viewer-bookmarks-content'});
---
+++ pdf/pdf_fitting_type.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Enumeration of page fitting types.
+ * @enum {string}
+ */
+const FittingType = {
+  NONE: 'none',
+  FIT_TO_PAGE: 'fit-to-page',
+  FIT_TO_WIDTH: 'fit-to-width',
+  FIT_TO_HEIGHT: 'fit-to-height'
+};
